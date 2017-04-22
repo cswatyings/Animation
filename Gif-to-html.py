c5f0a090f440f07bb01431853e0f38de89f190eb
Siwei Chen <schen157@fordham.edu>
2017-04-22 15:57:58 -0400

    Create Gif-to-html.py

---
+++ Gif-to-html.py
@@ -0,0 +1,11 @@
+import io
+import base64
+from matplotlib import animation, rc
+from IPython.display import HTML
+
+
+filename = '~/DC.gif'
+
+video = io.open(filename, 'r+b').read()
+encoded = base64.b64encode(video)
+HTML(data='''<img src="data:image/gif;base64,{0}" type="gif" />'''.format(encoded.decode('ascii')))
